aa0ad577228eb4583934054a08a1b42a963ac428
karthikmca39 <karthikmca39@users.noreply.github.com>
2018-06-24 12:17:40 +0530

    Update Plot1.R

--- Plot1.R
+++ Plot1.R
@@ -14,3 +14,4 @@ sub<- subset(elet, (elet$Date == "1/2/2007" | elet$Date== "2/2/2007"))
 png("plot1.png", width=480, height= 480)
 hist(sub$Global_active_power, col= "red", xlab= "Global Active Power (kilowatts)", ylab= "Frequency", main= "Global Active Power")
 dev.off()
+
