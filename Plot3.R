d757ddedc7d38cc8fc9308aa2401233b61d7d8e0
karthikmca39 <karthikmca39@users.noreply.github.com>
2018-06-24 12:14:28 +0530

    Update Plot3.R

--- Plot3.R
+++ Plot3.R
@@ -22,3 +22,4 @@ lines(sub$DateTime, sub$Sub_metering_2, type="l", col="red")
 lines(sub$DateTime, sub$Sub_metering_3, type="l", col="blue")
 legend("topright", c("Sub_metering_1", "Sub_metering_2", "Sub_metering_3"), lty=1, col=c("black", "red", "blue"))
 dev.off()
+
